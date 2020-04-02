9cb61c201f7eaee926fedd101f1e1d6c6de72294
Maya Soloveva <maya.soloveva2015@yandex.ru>
2020-04-02 15:04:46 +0300

    Routing

--- http/src/containers/Blog/Blog.js
+++ http/src/containers/Blog/Blog.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import axios from '../../axios';
 
 import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -54,14 +52,16 @@ class Blog extends Component {
         }
         return (
             <div>
+                <header className = "Blog">
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedId}/>
-                </section>
-                <section>
-                    <NewPost />
                 </section>
             </div>
         );
